9ccac6e11c74599b23a2a208480d7f0f7dfdb898
cyuber.devoloper <sabdumajid91@gmail.com>
2026-01-01 12:50:44 +0500

    yangi versiya admin panelv korinnish biroz ozgardi

--- course/admin.py
+++ course/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from  .models import *
 
 admin.site.register(Branch)
-admin.site.register(Infaration)
 admin.site.register(Burning)
+
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ['branch_name']
+
+@admin.register(Infaration)
+class InfarationAdmin(admin.ModelAdmin):
+    list_display = ['young', 'phone_number', 'telegram_urls', 'created_at']
+    filter_horizontal = ['select_branch', 'direction']
+
